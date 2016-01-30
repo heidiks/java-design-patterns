e624a7bd27d9675af6aa23071a96c8f5a55d2544
Heidi <heidiks@gmail.com>
2016-01-30 11:05:45 -0200

    Migrating

--- src/com/github/heidiks/chainOfResponsibility/Desconto.java
+++
@@ -1,9 +0,0 @@
-package com.github.heidiks.chainOfResponsibility;
-
-import com.github.heidiks.model.Orcamento;
-
-public interface Desconto {
-
-	double desconto(Orcamento orcamento);
-	void setProximo(Desconto desconto);
-}
