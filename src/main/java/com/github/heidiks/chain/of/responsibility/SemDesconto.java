328afb97924cec43d7fc112c1d0715768cac19e8
Heidi <heidiks@gmail.com>
2016-02-06 21:45:51 -0200

    Refactoring

--- src/main/java/com/github/heidiks/chain/of/responsibility/SemDesconto.java
+++ src/main/java/com/github/heidiks/chain/of/responsibility/SemDesconto.java
@@ -3,17 +3,7 @@ package com.github.heidiks.chain.of.responsibility;
 import com.github.heidiks.model.Orcamento;
 
 public class SemDesconto implements Desconto {
-	
-	private Desconto desconto;
 
-	public SemDesconto(Desconto desconto) {
-		this.desconto = desconto;
-	}
-
-	public SemDesconto() {
-		this.desconto = null;
-	}
-	
 	@Override
 	public Double desconto(Orcamento orcamento) {
 		return 0d;
